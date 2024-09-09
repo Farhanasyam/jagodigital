73d5e359c0b00b8551738af24246ad501483d180
Muh. Rafqi Putra <kenb.space3112@gmail.com>
2024-09-09 17:30:34 +0700

    fill in migration content-planner

--- app/Database/Migrations/2024-09-09-094114_CreateContentTypeTable.php
+++ app/Database/Migrations/2024-09-09-094114_CreateContentTypeTable.php
@@ -8,11 +8,24 @@ class CreateContentTypeTable extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id_content_type' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'nama_content_type' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+            ],
+        ]);
+        $this->forge->addKey('id_content_type', true);
+        $this->forge->createTable('content_type');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('content_type');
     }
 }
